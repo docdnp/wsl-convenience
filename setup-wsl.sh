99b779dff41871f1af4f89ef1403e751bc6c0f17
docdnp <contactme@via-github.please>
2023-03-14 12:53:54 +0100

    added souces of information and make use of newer docker versions

--- setup-wsl.sh
+++ setup-wsl.sh
@@ -2,29 +2,48 @@
 # set -x 
 set -e 
 
+# =======================================================================================================================
+# HELPER FUNCTIONS 
+# =======================================================================================================================
+distro   () { cat /etc/*-release  | grep ^ID=       | sed -re 's|ID=||' ; }
+parent   () { cat /etc/*-release  | grep ^ID_LIKE=  | sed -re 's|ID=||' ; }
 unixpath () { sed -re 's|\\|/|g' -e 's|[\r\n]||g' -e 's| |\\ |g' ; }
 wintool  () { wslpath -u "$(/mnt/c/Windows/system32/where.exe "$@" 2>/dev/null | head -1 )" 2>/dev/null | unixpath; }
-installed() { command -v "$1" >&/dev/null; }
 contains () { grep "$1" >&/dev/null; }
+installed() { command -v "$1" >&/dev/null; }
+havepkg  () { dpkg -l "$1" | contains ^ii; }
 mkwrapper() { echo "Installing win tool wrapper: $1"; echo -e "#!/bin/bash\n$2 \"\$@\"" > ~/.local/bin/$1; chmod +x ~/.local/bin/$1; }
 wslcfgget() { crudini --get /etc/wsl.conf "$1" "$2" ; }
 wslcfgset() { echo "Setting WSL config: $1.$2 = $3"; crudini --set /etc/wsl.conf "$@" ; }
 sudofunc () { local func=$1; shift; sudo bash -c "$(declare -f $func); $func \"$1\" \"$2\" \"$3\"" ; }
 
-# ======================================================================================================
+# =======================================================================================================================
+# CHECK PRECONDITIONS:
+#  * We are on disro based on debian
+#  * Need 'crudini', 'curl'
+#  * Want bash-completion
+#  * Want latest PowerShell
+# =======================================================================================================================
 echo "Checking preconditions... "
+{ distro; parent; } | contains debian || {
+    echo "Error: Tool 'crudini' is missing. Only debian based distributions are supported."
+    echo "If you install 'crudini' manually, the script will continue."
+    exit 1
+}
+DISTRO=$(distro)
+
 installed crudini || {
-    installed apt || { 
-        echo "Error: Tool 'crudini' is missing. Only debian based distributions are supported."
-        echo "If you install 'crudini' manually, the script will continue."
-        exit 1
-    }
+    apt update
     echo "Installing helper tool: crudini"
     sudo apt install -y crudini
 }
-installed curl || sudo apt install -y curl
+installed curl              || sudo apt install -y curl
+havepkg   bash-completion   || sudo apt install -y bash-completion
 
-# ======================================================================================================
+winget list | contains Microsoft.PowerShell || winget install Microsoft.PowerShell
+# =======================================================================================================================
+# GET WINHOME
+# =======================================================================================================================
 echo -n "Checking HOME under Windows... "
 POWERSHELL="$(wintool pwsh.exe)"
 [ -z "$POWERSHELL" ] && POWERSHELL="$(wintool Powershell)"
@@ -34,7 +53,11 @@ echo "WINHOME=$WINHOME"
 mkdir -p $WINHOME/AppData/Local/Docker; 
 mkdir -p ~/.local/bin
 
-# ======================================================================================================
+# =======================================================================================================================
+# GIT WIN & LINUX: 
+#  * https://winget.run/pkg/Git/Git
+#  * https://github.com/git-guides/install-git
+# =======================================================================================================================
 WINGET="$(wintool winget.exe)"
 PKGS=(Git.Git GitHub.GitLFS Microsoft.GitCredentialManagerCore)
 $WINGET list |  perl -ne 'BEGIN{ @P=qw('"${PKGS[*]}"'); $p=join("|", @P)} /$p/ && $i++; END{ exit ($i == @P ? 0 : 1) }'
@@ -50,10 +73,13 @@ $WINGET list |  perl -ne 'BEGIN{ @P=qw('"${PKGS[*]}"'); $p=join("|", @P)} /$p/ &
 installed apt && {
     installed git || {
         echo "Installing Git for debian based linux..."
-        sudo apt install -y git
+        sudo apt install -y git-all 
     }
 }
 
+# =======================================================================================================================
+# GIT CREDENTIAL MGR: https://learn.microsoft.com/en-us/windows/wsl/tutorials/wsl-git
+# =======================================================================================================================
 GITCREDMGR="$(wintool /R 'C:\Program Files' git-credential-manager.exe)"
 WHOAMI=$(wintool whoami.exe)
 WINUSER=$($WHOAMI | sed -re 's/.*\\|\r|\n//g')
@@ -70,7 +96,11 @@ git config --global --get user.name | contains "$WINUSER" || {
     git config --global credential.helper ~/.local/bin/git-credential-manager
 }
 
-# ======================================================================================================
+# =======================================================================================================================
+# ACCESS OTHER DISTROS' FILES:
+#  * https://superuser.com/questions/1659218/is-there-a-way-to-access-files-from-one-wsl-2-distro-image-in-another-one
+#  * https://askubuntu.com/questions/1395780/accessing-ubuntu-wsl-files-from-kali-wsl-and-vice-versa/1395784#1395784
+# =======================================================================================================================
 cat /etc/fstab | contains /mnt/wsl/instances/$WSL_DISTRO_NAME || {
     echo "Setting up sharing of filesystems between different WSL distros."
     echo "/ /mnt/wsl/instances/$WSL_DISTRO_NAME none defaults,bind,X-mount.mkdir 0 0" \
@@ -82,16 +112,23 @@ wslcfgget boot      command           | contains sleep || sudofunc wslcfgset boo
 wslcfgget interop   appendWindowsPath | contains false || sudofunc wslcfgset interop appendWindowsPath false
 wslcfgget automount mountFsTab        | contains false || sudofunc wslcfgset automount mountFsTab false
 
-# ======================================================================================================
+# =======================================================================================================================
+# WSL HELPERS BASHRC: https://github.com/docdnp/wsl-convenience
+# =======================================================================================================================
 grep "added by setup-wsl.sh" ~/.bashrc >&/dev/null || {
 echo "Adding helpful stuff to ~/.bashrc"
 
+# Export WINHOME and basic aliases
 cat <<EOF >> ~/.bashrc
 ## Next lines were added by setup-wsl.sh
 export WINHOME="$WINHOME"
 export PATH=~/.local/bin:\$PATH
 alias where=/mnt/c/Windows/system32/where.exe
 alias pwsh=$POWERSHELL
+EOF
+
+# Provide a 'which' like tool for windows executables
+cat <<EOF >> ~/.bashrc
 __wsl_which_usage () {
 cat <<EOT
 Usage: wsl-which [OPTS|GREP]
@@ -124,7 +161,11 @@ wsl-which () {
     } | sort -u > \$whichcache
     grep --color=never -E\$grepi ".*/\$pattern" \$whichcache 
 }
+EOF
 
+# Provide a tool to create aliases or function wrappers mostly for windows
+# executables 
+cat <<EOF >> ~/.bashrc
 to-alias () { 
     local aliases=\${1:-~/.bash_aliases}
     perl -pe '
@@ -150,7 +191,10 @@ to-alias () {
                 )
              }ex' | tee /dev/stderr >> \$aliases
 }
+EOF
 
+# Provide an 'uninstaller'
+cat <<EOF >> ~/.bashrc
 wsl-convenience-uninstall () {
     local PKGS=(docker-credential-wincred
                 git-credential-manager
@@ -162,7 +206,15 @@ wsl-convenience-uninstall () {
     sed -i '/^## Next.*setup-wsl.sh/,/^## Prev.*setup-wsl.sh/{//!d}' ~/.bashrc;
     sed -ire 's/^.*setup-wsl.sh.*//g' ~/.bashrc
 } 
+EOF
 
+# Open duplicated tabs of a distro in the current directory
+# of another distro and set tab title: 
+#
+#  * https://learn.microsoft.com/en-us/windows/terminal/tutorials/new-tab-same-directory
+#  * https://learn.microsoft.com/en-us/windows/terminal/tutorials/tab-title
+#
+cat <<EOF >> ~/.bashrc
 my_prompt () { printf "\e]9;9;%s\e\\\\" "\$(wslpath -w "\$PWD")" ; }
 export PROMPT_COMMAND=my_prompt
 
@@ -173,7 +225,9 @@ PS1="\[\e]0;\${WSL_DISTRO_NAME}: \w\a\]\$PS1"
 EOF
 }
 
-# ======================================================================================================
+# =======================================================================================================================
+# INPUTRC: https://github.com/docdnp/inputrc
+# =======================================================================================================================
 curl https://raw.githubusercontent.com/docdnp/inputrc/main/shell.inputrc -o /tmp/.inputrc >&/dev/null
 
 if [ -e ~/.inputrc ] ; then 
@@ -187,7 +241,9 @@ else
     mv /tmp/.inputrc ~/.inputrc
 fi
 
-# ======================================================================================================
+# =======================================================================================================================
+# Docker: https://docs.docker.com/engine/install/
+# =======================================================================================================================
 DOCKERCRED="$WINHOME/AppData/Local/Docker/docker-credential-wincred"
 ! [ -e "$DOCKERCRED" ] && {
     echo "Setting up docker and docker credential helper."
@@ -200,20 +256,36 @@ DOCKERCRED="$WINHOME/AppData/Local/Docker/docker-credential-wincred"
 
 RESTART=false
 
-dpkg -l docker.io | contains "^ii" || {
-    echo "Installing docker.io..."
-    sudo apt install -y docker.io bash-completion
+havepkg docker.io && {
+    echo "Uninstalling docker-io..."
+    sudo apt -y remove docker docker-engine docker.io containerd runc
+}
+
+havepkg docker-ce || {
+    echo "Installing docker-ce..."
+    sudo apt update
+    sudo apt install -y ca-certificates curl gnupg lsb-release
+    sudo mkdir -p /etc/apt/keyrings
+
+    curl -fsSL https://download.docker.com/linux/${DISTRO}/gpg \
+        | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/${DISTRO} \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
     sudo usermod -a -G docker $USER
 }
 
-mkdir -p ~/.docker
 cat ~/.docker/config.json | contains 'credStore.*wincred' || {
     echo "Setting up credential store..."
     echo -e "{\n  \"credStore\": \"wincred\"\n}" > ~/.docker/config.json
     RESTART=true
 }
 
-# ======================================================================================================
+# =======================================================================================================================
 echo "Setting up neccessary or useful windows tool wrappers..."
 WSL=$(wintool wsl.exe)
 installed wsl                       || mkwrapper wsl                       "$WSL"
@@ -222,7 +294,7 @@ installed git-credential-manager    || mkwrapper git-credential-manager    "$GIT
 installed docker-credential-wincred || mkwrapper docker-credential-wincred "$DOCKERCRED"
 installed winget                    || mkwrapper winget                    "$WINGET"
 
-# ======================================================================================================
+# =======================================================================================================================
 $RESTART && {
     echo "Going to shutdown $WSL_DISTRO_NAME in 5 seconds... Please restart manually..."
     echo "After restart do: docker login registry.your.local.one"
